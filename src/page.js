b3eae020e45c612cf195246c2e41aeea94791616
Benjamin Eidelman <beneidel@gmail.com>
2014-12-30 11:59:04 -0200

    fixing page visit

--- src/page.js
+++ src/page.js
@@ -18,35 +18,42 @@ Page.prototype.visit = function(params, callback) {
 
   // compose a target url
   var targetUrl = this.route;
+  if (!targetUrl) {
+    throw new Error('this page has no specified route');
+  }
   if (targetUrl.substr(0, 1) === '/') {
     targetUrl = url.resolve(this.config.baseUrl, targetUrl);
   }
-  var usedParams = {};
-  targetUrl = targetUrl
-    .replace(/\*.*$/, '')
-    .replace(/\:[a-z0-9]+[\?]?/g, function(match){
-      var name = match.substr(1, match.length - 2 -
-        (match.substr(match.length - 1) === '?') ? 1 : 0);
-      if (params && params[name]) {
-        usedParams[name] = true;
-        return params[name];
+
+  if (params && typeof params === 'object') {
+
+    var usedParams = {};
+    targetUrl = targetUrl
+      .replace(/\*.*$/, '')
+      .replace(/\:[a-z0-9]+[\?]?/g, function(match) {
+        var name = match.substr(1, match.length - 2 -
+          (match.substr(match.length - 1) === '?') ? 1 : 0);
+        if (params && params[name]) {
+          usedParams[name] = true;
+          return params[name];
+        }
+        return '';
+      })
+      .replace(/\/[\/]+/g, '/');
+
+    // additional query params
+    Object.keys(params).forEach(function(name) {
+      if (name === '_hash' || usedParams[name]) {
+        return;
       }
-      return '';
-    })
-    .replace(/\/[\/]+/g, '/');
+      targetUrl += (targetUrl.indexOf('?') < 0) ? '?' : '&';
+      var value = params[name];
+      targetUrl += name + '=' + encodeURIComponent(value);
+    });
 
-  // additional query params
-  Object.keys(params).forEach(function(name) {
-    if (name === '_hash' || usedParams[name]) {
-      return;
+    if (params._hash) {
+      targetUrl += '#' + params._hash;
     }
-    targetUrl += (targetUrl.indexOf('?') < 0) ? '?' : '&';
-    var value = params[name];
-    targetUrl += name + '=' + encodeURIComponent(value);
-  });
-
-  if (params._hash) {
-    targetUrl += '#' + params._hash;
   }
 
   this.browser.url(targetUrl).call(callback);
